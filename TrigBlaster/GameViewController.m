c934b03d9a86828d3f9d70b1a6e9feaaae504b14
Michael Vilabrera <m.vilabrera@gmail.com>
2018-05-16 15:25:16 -0400

    eliminate warning artifacts

--- TrigBlaster/GameViewController.m
+++ TrigBlaster/GameViewController.m
@@ -66,7 +66,7 @@
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         return UIInterfaceOrientationMaskAllButUpsideDown;
